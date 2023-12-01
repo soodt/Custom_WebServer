5ff5a41e34f6c9ad2c08abe93096e295cb7f541e
Tanuj Sood <113442573+soodt@users.noreply.github.com>
2023-11-30 19:36:16 -0800

    Update part2.py

--- final/part2.py
+++ final/part2.py
@@ -16,7 +16,7 @@ def log_text(response):
     status_line = response.split(b"\r\n")[0].decode()
     headers = response.decode().split("\r\n\r\n")[0]
     with open('tasoodHTTPResponses.txt', 'a') as file:
-        file.write(f"{status_line}\n{headers}\n\n")
+        file.write("{}\n{}\n".format(status_line,headers))
 
 
 def getType(extension):
@@ -54,25 +54,25 @@ def http_response(path):
         extension = os.path.splitext(path)[1].lower()
         type = getType(extension)
         http_message = (
-        f"HTTP/1.1 200 OK\r\n" 
-        f"Content-Length: {content_length}\r\n" 
-        f"Content-Type: {type}\r\n"
-        f"Date: {datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')} \r\n"
-        f"Last-Modified: {last_modified}\r\n"
-        f"Connection: close\r\n"
-        f"\r\n"
-        )
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Length: {}\r\n" 
+        "Content-Type: {}\r\n"
+        "Date: {} \r\n"
+        "Last-Modified: {}\r\n"
+        "Connection: close\r\n"
+        "\r\n"
+        ).format(content_length,type,datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT'),last_modified)
         http_message = http_message.encode() + file_content + b"\r\n"
         #sys.stdout.write(http_message)
        
     else:
         http_message = (
-            f"HTTP/1.1 404 Not Found\r\n"
-            f"Date: {datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\n"
-            f"Connection: close\r\n"
-            f"\r\n"
-            f"404 Not Found: File not found\r\n"
-        )
+            "HTTP/1.1 404 Not Found\r\n"
+            "Date: {}\r\n"
+            "Connection: close\r\n"
+            "\r\n"
+            "404 Not Found: File not found\r\n"
+        ).format(datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT'))
     return http_message, content_length
 
 
@@ -87,7 +87,7 @@ port_num = args.p
 dir_path = args.d
 
 port_num = 8000
-dir_path = "/Users/tanujsood/Desktop"
+# dir_path = "/Users/tanujsood/Desktop"
 
 if port_num == 8000:
     #sys.stdout.write(f"{port_num} {dir_path}\n")
@@ -97,51 +97,48 @@ if port_num == 8000:
     socket_info = serverSocket.getsockname()
     socket_ip = socket_info[0]
     socket_port = socket_info[1]
-    sys.stdout.write(f"Welcome socket created: {socket_ip}, {socket_port}\n")
-    print(" ")
+    sys.stdout.write("Welcome socket created: {}, {}\n".format(socket_ip,socket_port))
     while True:
         connectionSocket, addr = serverSocket.accept()
-        sys.stdout.write(f"Connection socket created: {addr[0]}, {addr[1]}\n")
+        sys.stdout.write("Connection socket created: {}, {}\n".format(addr[0],addr[1]))
         sentence = connectionSocket.recv(1024).decode()
         sentence_split = sentence.split()
         if sentence_split[0] != "GET":
             error_response = (
-            f"HTTP/1.1 501 Not Implemented\r\n\r\n"
-            f"Date: {datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\n"
-            f"Connection: close\r\n"
-            f"\r\n"
-            f"501 Not Implemented: The server does not support this functionality r\n"
-            )
+            "HTTP/1.1 501 Not Implemented\r\n\r\n"
+            "Date: {}\r\n"
+            "Connection: close\r\n"
+            "\r\n"
+            "501 Not Implemented: The server does not support this functionality r\n"
+            ).format(datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT'))
             connectionSocket.close()
-            sys.stdout.write(f"Connection to {addr[0]}, {addr[1]} is now closed.")
+            sys.stdout.write("Connection to {}, {} is now closed.".format(addr[0],addr[1]))
         elif sentence_split[2] != "HTTP/1.1":
             error_response = (
-            f"HTTP/1.1 505 HTTP Version Not Supported\r\n"
-            f"Date: {datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\n"
-            f"Connection: close\r\n"
-            f"\r\n"
-            f"505 HTTP Version Not Supported: The server only supports HTTP/1.1\r\n"
-            )
+            "HTTP/1.1 505 HTTP Version Not Supported\r\n"
+            "Date: {}\r\n"
+            "Connection: close\r\n"
+            "\r\n"
+            "505 HTTP Version Not Supported: The server only supports HTTP/1.1\r\n"
+            ).format(datetime.utcnow().strftime('%a, %d %b %Y %H:%M:%S GMT'))
             connectionSocket.close()
-            sys.stdout.write(f"Connection to {addr[0]}, {addr[1]} is now closed.")
+            sys.stdout.write("Connection to {}, {} is now closed.".format(addr[0]),addr[1])
         else:
             path = os.path.join(dir_path, sentence_split[1].lstrip("/"))
-            response, length = http_response(dir_path)
+            response, length = http_response(path)
             connectionSocket.send(response)
             connectionSocket.close()
+            sys.stdout.write("Connection to {}, {} is now closed.".format(addr[0],addr[1]))
             log_csv(socket_ip, socket_port, addr[0], addr[1], sentence_split[1], response.split(b"\r\n")[0].decode(),
                         length)
             log_text(response)
-            sys.stdout.write(f"Connection to {addr[0]}, {addr[1]} is now closed.")
             
 elif port_num in range(0,1024):
-    sys.stdout.write(f"Well-known port number {port_num} entered - could cause a conflict.\n")
-    sys.stdout.write(f"{port_num} {dir_path}\n")
+    sys.stdout.write("Well-known port number {} entered - could cause a conflict.\n".format(port_num))
+    sys.stdout.write("{} {}\n".format(port_num,dir_path))
 elif port_num in range(1024, 49152):
-    sys.stdout.write(f"Registered port number {port_num} entered - could cause a conflict.\n")
-    sys.stdout.write(f"{port_num} {dir_path}\n")
+    sys.stdout.write("Registered port number {} entered - could cause a conflict.\n".format(port_num))
+    sys.stdout.write("{} {}\n".format(port_num,dir_path))
 else:
     sys.stderr.write("Terminating program, port number is not allowed.\n")
     sys.exit(1)
-
-
